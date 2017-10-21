7533d9d1161af99e7d09986298096b7cc9b296c7
andreybozhkov <bozhkov.andrey@gmail.com>
2017-10-22 01:51:37 +0300

    ADDED bare starting views index and about

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     development: {
         port: process.env.PORT || 27017,
-        dbPath: 'mongodb://localhost:27017/db'
+        dbPath: 'mongodb://localhost:27017/Test-Database'
     },
     production: {}
 };
